d027d063c7da28b6dee03d19301e05432f917556
matheus0214 <matheusgiga123@gmail.com>
2021-11-01 19:56:23 -0300

    max heap

---
+++ heap/heap-sort.js
@@ -0,0 +1,42 @@
+import { compare, swap } from "../utils/index.js";
+
+const COMPARE = {
+    LESS_THAN: -1,
+    GREATER_THAN: 1,
+};
+
+export default function heapSort(array, compareFn = compare) {
+    let heapSize = array.length;
+    buildMaxHeap(array, compareFn);
+
+    while (heapSize > 1) {
+        swap(array, 0, --heapSize);
+        heapify(array, 0, heapSize, compareFn);
+    }
+
+    return array;
+}
+
+function buildMaxHeap(array, compareFn) {
+    for (let i = Math.floor(array.length / 2); i >= 0; i -= 1) {
+        heapify(array, i, array.length, compareFn);
+    }
+
+    return array;
+}
+
+function heapify(array, index, heapSize, compareFn) {
+    let largest = index;
+    const left = 2 * index + 1;
+    const right = 2 * index + 2;
+    if (left < heapSize && compareFn(array[left], array[index]) > 0) {
+        largest = left;
+    }
+    if (right < heapSize && compareFn(array[right], array[largest]) > 0) {
+        largest = right;
+    }
+    if (largest !== index) {
+        swap(array, index, largest);
+        heapify(array, largest, heapSize, compareFn);
+    }
+}
